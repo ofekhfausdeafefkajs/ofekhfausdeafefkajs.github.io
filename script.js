2de7a628354c9238ef429015c34bd49fa720b2cf
ofekhfausdeafefkajs <38990733+ofekhfausdeafefkajs@users.noreply.github.com>
2018-05-10 16:08:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+ var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+
+var yyyy = today.getFullYear();
+if(dd<10){
+    dd='0'+dd -1;
+} 
+if(mm<10){
+    mm='0'+mm;
+} 
+var today = dd+'.'+mm+'.'+yyyy;
+document.getElementById("date").innerHTML = today;
+
+
+// test();
+// function test(){
+//     var number1 = (Math.round(parseInt(Date.now().toString().substr(4,6))/2));
+//     setTimeout(test,2000);
+//     document.getElementById("count").innerHTML = number1;
+// }
+});
+
