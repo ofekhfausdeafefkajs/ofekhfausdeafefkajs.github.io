36afc5f9ccd011a6fd3b0ad1f178d69dd9da31eb
ofekhfausdeafefkajs <38990733+ofekhfausdeafefkajs@users.noreply.github.com>
2018-06-06 07:23:55 +0700

    Create testphp.php

---
+++ testphp.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1><?php echo "Пример встраивания PHP"; ?></h1>
+        <p>Обычный не обработанный интерпретатором текст</p>
+    </body>
+</html>
